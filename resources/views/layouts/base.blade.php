d2bf3b6583a245d37a67e5140a7a411021f35557
Bryan de Knikker <bdeknikker04@gmail.com>
2024-08-28 21:28:53 +0200

    New files for redesign

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,55 +1,35 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('pageTitle') - Bryan de Knikker</title>
-    <meta name="description" content="@yield('metaDescription')">
+    <!-- Begin pagina titel -->
+    <title>@yield('pageTitle', 'Bryan de Knikker') - Bryan de Knikker</title>
+    <!-- Einde pagina titel -->
+
+    <!-- Begin Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/css/lightbox.min.css" rel="stylesheet">
+    <!-- Einde Bootstrap CSS -->
+
+    <!-- Begin eigen CSS -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="icon" type="image/x-icon" href="{{URL('/images/BK-favicon.svg')}}">
-    <link rel="canonical" href="{{ url()->current() }}" />
-    <meta property="og:title" content="@yield('opengraphMetaTitle')">
-    <meta property="og:description" content="@yield('opengraphMetaDescription')">
-    <meta property="og:url" content="@yield('opengraphMetaUrl')">
-    <meta property="og:image" content="@yield('opengraphMetaImage')">
-    <!-- Google Tag Manager -->
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-P4WLP96K');</script>
-    <!-- End Google Tag Manager -->
+    <!-- Einde eigen CSS -->
 </head>
 <body>
-<!-- Google Tag Manager (noscript) -->
-<noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4WLP96K"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
-<!-- End Google Tag Manager (noscript) -->
-@include('includes.header')
+<!-- Begin algemene header -->
+@include('partials.header')
+<!-- Einde algemene header -->
 
-@yield('content')
+<!-- Begin pagina-specifieke content -->
+<div class="content">
+    @yield('content')
+</div>
+<!-- Einde pagina-specifieke content -->
 
-@include('includes.footer')
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox-plus-jquery.min.js"></script>
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-<script src="{{ asset('js/app.js') }}"></script>
-
+<!-- Begin algemene footer -->
+@include('partials.footer')
+<!-- Einde algemene footer -->
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,48 +12,4 @@ Route::get('/', function () {
     return view('pages.home');
 })->name('home');
 
-Route::get('/contact/', function () {
-    return view('pages.contact');
-})->name('contact');
 
-Route::get('/over-mij/', function () {
-    return view('pages.about');
-})->name('over-mij');
-
-Route::get('/cases/', function () {
-    return view('pages.cases');
-})->name('cases');
-
-Route::get('/cases/accessibility/', function () {
-    return view('pages.accessibility');
-})->name('accessibility');
-
-Route::get('/cases/kunst-van-elk/', function () {
-    return view('pages.kunstvanelk');
-})->name('kunstvanelk');
-
-Route::get('/cases/test-correct/', function () {
-    return view('pages.testcorrect');
-})->name('testcorrect');
-
-Route::get('/cases/pixels-gunner/', function () {
-    return view('pages.pixelsgunner');
-})->name('pixelsgunner');
-
-Route::get('/cases/glitch/', function () {
-    return view('pages.glitch');
-})->name('glitch');
-
-Route::get('/cases/readyfortravelling/', function () {
-    return view('pages.readyfortravelling');
-})->name('readyfortravelling');
-
-Route::get('/cases/magazijnlade/', function () {
-    return view('pages.magazijnlade');
-})->name('magazijnlade');
-
-Route::get('/contact/', [ContactController::class, 'contact'])->name('contact');
-
-Route::post('/contact/', [ContactController::class, 'contactPost'])->name('contact.store');
-
-Route::get('/sitemap.xml', [SitemapController::class, 'generate'])->name('sitemap');
