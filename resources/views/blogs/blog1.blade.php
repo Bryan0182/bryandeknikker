cf1807470afd3ac55081bb5dead9396705d68304
Bryan0182 <bdeknikker04@gmail.com>
2024-08-30 11:58:37 +0200

    Deleted files

--- resources/views/blogs/blog1.blade.php
+++
@@ -1,8 +0,0 @@
-@php
-    $date = '2024-08-22';
-@endphp
-<article>
-    <h2>Blog Titel 1</h2>
-    <p>Inhoud van de blog...</p>
-    <small>Gepubliceerd op {{ $date }}</small>
-</article>
--- resources/views/blogs/blog2.blade.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/blogs/blog2.blade.php -->
-@php
-    $date = '2024-08-24';
-@endphp
-<article>
-    <h2>Blog Titel 1</h2>
-    <p>Inhoud van de blog...</p>
-    <small>Gepubliceerd op {{ $date }}</small>
-</article>
