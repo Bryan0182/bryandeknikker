d70d05313a13ba563724d7760d948770a75853c4
Bryan0182 <bdeknikker04@gmail.com>
2024-08-30 11:59:44 +0200

    Changed hero section

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -2,7 +2,7 @@
     <div class="row align-items-center g-5 py-5 hero-row">
         <div class="col-lg-6">
             <h1 class="fw-bold text-body-emphasis lh-1">{{ $title }}</h1>
-            <p class="lead description-paragraph">{{ $description }}</p>
+            <p class="description-paragraph">{{ $description }}</p>
             <div class="d-grid gap-2 d-md-flex justify-content-md-start button-hero-row">
                 <button type="button" class="btn btn-outline-primary btn-lg px-4 me-md-2 contact-hero-button">{{ $first_button }}</button>
                 <button type="button" class="btn btn-primary btn-lg px-4 align-items-center d-flex read-more-hero-button">{{ $second_button }}</button>
