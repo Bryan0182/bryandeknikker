d43722a701817812e691ca4a2674dc32d84c1518
Bryan0182 <bdeknikker04@gmail.com>
2024-08-30 15:29:13 +0200

    Changed hero with new variables

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -4,8 +4,8 @@
             <h1 class="fw-bold text-body-emphasis lh-1">{{ $title }}</h1>
             <p class="description-paragraph">{{ $description }}</p>
             <div class="d-grid gap-2 d-md-flex justify-content-md-start button-hero-row">
-                <button type="button" class="btn btn-outline-primary btn-lg px-4 me-md-2 contact-hero-button">{{ $first_button }}</button>
-                <button type="button" class="btn btn-primary btn-lg px-4 align-items-center d-flex read-more-hero-button">{{ $second_button }}</button>
+                <a href="{{ $first_button_url }}" class="btn btn-outline-primary btn-lg px-4 me-md-2 contact-hero-button">{{ $first_button }}</a>
+                <a href="{{ $second_button_url }}"  class="btn btn-primary btn-lg px-4 align-items-center d-flex read-more-hero-button">{{ $second_button }}</a>
             </div>
         </div>
         <div class="col-12 col-sm-8 col-lg-6">
