d25c320ecc67074fc8fc1de2bef073f62f8a12d5
Bryan0182 <bdeknikker04@gmail.com>
2024-08-30 11:59:58 +0200

    Changed urls

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BlogController;
 
-Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/recent-blogs', [BlogController::class, 'showRecentBlogs'])->name('recent-blogs');
+Route::get('/blogs/create', function () {
+    return view('pages.blog_create');
+})->name('blogs.create');
+
+Route::get('/blogs/success', [BlogController::class, 'success'])->name('blogs.success');
+
+Route::post('/blogs', [BlogController::class, 'store'])->name('blogs.store');
+
+Route::get('/recent-blogs', [BlogController::class, 'showRecentBlogs'])->name('blogs.recent');
+
+Route::get('/blogs/{slug}', [BlogController::class, 'show'])->name('blogs.show');
