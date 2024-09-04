ef1a76ed9dae7b449253fd0cccea819dee55529c
Bryan0182 <bdeknikker04@gmail.com>
2024-09-04 19:45:25 +0200

    Changes for home page with new skills block

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/hero.css') }}">
     <link rel="stylesheet" href="{{ asset('css/recent_blogs.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/skills.css') }}">
 @endpush
 
 @section('content')
@@ -24,4 +25,33 @@
     @endcomponent
 
     @include('components.recent_blogs', ['recentBlogs' => $recentBlogs])
+
+    @component('components.skills', [
+        'frontend' => [
+            'languages' => [
+                ['name' => 'HTML', 'icon' => 'html5-logo.svg'],
+                ['name' => 'CSS', 'icon' => 'css3-logo.svg'],
+                ['name' => 'Sass', 'icon' => 'sass-logo.svg'],
+                ['name' => 'JS', 'icon' => 'js-logo.svg'],
+            ],
+            'frameworks' => [
+                ['name' => 'Bootstrap', 'icon' => 'bootstrap-logo.svg'],
+                ['name' => 'React', 'icon' => 'react-logo.svg'],
+                ['name' => 'JQuery', 'icon' => 'jquery-logo.svg'],
+            ],
+        ],
+        'backend' => [
+            'languages' => [
+                ['name' => 'PHP', 'icon' => 'php-logo.svg'],
+                ['name' => 'Python', 'icon' => 'python-logo.svg'],
+            ],
+            'frameworks' => [
+                ['name' => 'Laravel', 'icon' => 'laravel-logo.svg'],
+                ['name' => 'Symfony', 'icon' => 'symfony-logo.svg'],
+                ['name' => 'Django', 'icon' => 'django-logo.svg'],
+                ['name' => 'Flask', 'icon' => 'flask-logo.svg'],
+            ],
+        ]
+    ])
+    @endcomponent
 @endsection
