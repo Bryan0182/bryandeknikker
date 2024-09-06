e450e0f97cbad73b2d806c953c5117c5460e03a5
Bryan0182 <bdeknikker04@gmail.com>
2024-09-06 19:59:45 +0200

    Added text section

---
+++ resources/views/components/text.blade.php
@@ -0,0 +1,8 @@
+<div class="container blog-detail-challenge-section">
+    <div class="row text-center g-5 py-5">
+        <div class="col-12">
+            <h2>{{ $title }}</h2>
+            <p>{{ $description }}</p>
+        </div>
+    </div>
+</div>
