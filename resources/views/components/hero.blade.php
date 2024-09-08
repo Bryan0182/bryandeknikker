7a1f89be79f3bed669732f29a5c7673f6293fa98
Bryan0182 <bdeknikker04@gmail.com>
2024-09-08 13:54:44 +0200

    Changes for hero block

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,5 +1,5 @@
 <div class="container hero-container col-12">
-    <div class="row align-items-center g-5 py-5 hero-row">
+    <div class="row align-items-center py-5 hero-row">
         <div class="col-lg-6">
             <h1 class="fw-bold text-body-emphasis lh-1">{{ $title }}</h1>
             <p class="description-paragraph">{{ $description }}</p>
@@ -8,8 +8,8 @@
                 <a href="{{ $second_button_url }}"  class="btn btn-primary btn-lg px-4 align-items-center d-flex read-more-hero-button">{{ $second_button }}</a>
             </div>
         </div>
-        <div class="col-12 col-sm-8 col-lg-6">
-            <img src="{{ $imageSrc }}" class="d-block mx-auto ms-md-auto me-md-0 img-fluid" alt="{{ $altText }}" width="{{ $width }}" height="{{ $height }}" loading="eager">
+        <div class="col-12 col-lg-6 mt-tablet">
+            <img src="{{ $imageSrc }}" class="d-block mx-auto ms-md-auto me-lg-0 img-fluid" alt="{{ $altText }}" width="{{ $width }}" height="{{ $height }}" loading="eager">
         </div>
     </div>
 </div>
