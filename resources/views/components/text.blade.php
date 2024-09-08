66062c4fcc8883c6a3c5fb5b1c59519bbaff699c
Bryan0182 <bdeknikker04@gmail.com>
2024-09-08 11:26:35 +0200

    Changes for text section

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -1,5 +1,5 @@
-<div class="container blog-detail-challenge-section">
-    <div class="row text-center g-5 py-5">
+<div class="container text-section">
+    <div class="row text-center py-5">
         <div class="col-12">
             <h2>{{ $title }}</h2>
             <p>{{ $description }}</p>
