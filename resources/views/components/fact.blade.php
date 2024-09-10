ec134f3d94e13bdb96f29f087597f819de9730fa
Bryan0182 <bdeknikker04@gmail.com>
2024-09-10 18:46:15 +0200

    Added fact block

---
+++ resources/views/components/fact.blade.php
@@ -0,0 +1,50 @@
+<div class="container-fluid fact-container-fluid">
+    <div class="container fact-container">
+        <div class="row align-items-start text-center">
+            <!-- Fact 1 -->
+            <div class="col-lg-4 mb-4">
+                <div class="card h-100 fact-card left">
+                    <div class="position-relative">
+                        <div
+                            class="position-absolute top-0 start-50 translate-middle bg-primary text-white rounded-circle p-3 fs-4 fw-bold fact-card-number">
+                            <img src="/images/1.svg" width="25px" height="35px"></div>
+                    </div>
+                    <div class="card-body mt-4">
+                        <h5 class="card-title fw-bold fact-card-title">{{ $fact1_title }}</h5>
+                        <p class="card-text fact-card-text">{{ $fact1_description }}</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Fact 2 -->
+            <div class="col-lg-4 mb-4">
+                <div class="card h-100 fact-card middle">
+                    <div class="position-relative">
+                        <div
+                            class="position-absolute top-0 start-50 translate-middle bg-primary text-white rounded-circle p-3 fs-4 fw-bold fact-card-number">
+                            <img src="/images/2.svg" width="25px" height="35px"></div>
+                    </div>
+                    <div class="card-body mt-4">
+                        <h5 class="card-title fw-bold fact-card-title">{{ $fact2_title }}</h5>
+                        <p class="card-text fact-card-text">{{ $fact2_description }}</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Fact 3 -->
+            <div class="col-lg-4 mb-4">
+                <div class="card h-100 fact-card right">
+                    <div class="position-relative">
+                        <div
+                            class="position-absolute top-0 start-50 translate-middle bg-primary text-white rounded-circle p-3 fs-4 fw-bold fact-card-number">
+                            <img src="/images/3.svg" width="25px" height="35px"></div>
+                    </div>
+                    <div class="card-body mt-4">
+                        <h5 class="card-title fw-bold fact-card-title">{{ $fact3_title }}</h5>
+                        <p class="card-text fact-card-text">{{ $fact3_description }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
