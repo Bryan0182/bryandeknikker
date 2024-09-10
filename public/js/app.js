3d076c838d6050c798ec279b7c97195b701bfa48
Bryan0182 <bdeknikker04@gmail.com>
2024-09-10 18:43:58 +0200

    Changes app javascript for slider

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.blog-detail-slider-images').slick({
+    $('.slider-images').slick({
         autoplay: true,
         autoplaySpeed: 5000,
         dots: true,
