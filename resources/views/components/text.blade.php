df37bd99d47cbffd2eb083eaca821e9874545ead
Bryan0182 <bdeknikker04@gmail.com>
2024-09-10 18:48:04 +0200

    Added text block

--- resources/views/components/text.blade.php
+++ resources/views/components/text.blade.php
@@ -2,7 +2,7 @@
     <div class="row text-center py-5">
         <div class="col-12">
             <h2>{{ $title }}</h2>
-            <p>{{ $description }}</p>
+            <p class="text-paragraph">{{ $description }}</p>
         </div>
     </div>
 </div>
