d014e88490a9ef39a0c81b3d1fbdb8e9d8ccd8ad
Bryan0182 <bdeknikker04@gmail.com>
2024-09-11 19:53:43 +0200

    Changes for global JS

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,32 @@ $(document).ready(function(){
         arrows: true,
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const themeSwitcher = document.getElementById('theme-switcher');
+    const iconSun = document.getElementById('icon-sun');
+    const iconMoon = document.getElementById('icon-moon');
+
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+        iconSun.style.display = 'block';
+        iconMoon.style.display = 'none';
+    } else {
+        iconSun.style.display = 'none';
+        iconMoon.style.display = 'block';
+    }
+
+    themeSwitcher.addEventListener('click', function() {
+        document.body.classList.toggle('dark-theme');
+
+        if (document.body.classList.contains('dark-theme')) {
+            localStorage.setItem('theme', 'dark');
+            iconSun.style.display = 'block';
+            iconMoon.style.display = 'none';
+        } else {
+            localStorage.setItem('theme', 'light');
+            iconSun.style.display = 'none';
+            iconMoon.style.display = 'block';
+        }
+    });
+});
