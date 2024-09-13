ef91f119990976e99822dc53b2eb670e3ac5f326
Bryan0182 <bdeknikker04@gmail.com>
2024-09-13 17:02:18 +0200

    Changes for header with theme color switch

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,9 @@
-<header class="text-bg-light header">
+<header class="header">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
             <a href="/" class="navbar-brand d-flex align-items-center">
-                <img src="{{ asset('/images/BK-logo.svg') }}" width="100" height="65" alt="Logo van Bryan de Knikker">
+                <img src="{{ asset('/images/BK-logo.svg') }}" class="logo-light" width="100" height="65" alt="Logo van Bryan de Knikker">
+                <img src="{{ asset('/images/BK-logo-white.svg') }}" class="logo-dark" width="100" height="65" alt="Logo van Bryan de Knikker">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,10 +12,10 @@
             <div class="collapse navbar-collapse" id="navbarNav" style="width: fit-content;">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item nav-item-header align-items-center d-flex">
-                        <a href="#" class="nav-link text-dark">Over mij</a>
+                        <a href="#" class="nav-link">Over mij</a>
                     </li>
                     <li class="nav-item nav-item-header align-items-center d-flex">
-                        <a href="#" class="nav-link text-dark">Skills</a>
+                        <a href="#" class="nav-link">Skills</a>
                     </li>
                 </ul>
                 <div class="d-flex flex-column flex-lg-row buttons-row">
