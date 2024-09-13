9cf93806eed796d0d52d5b9919e5d72515a60dc9
Bryan0182 <bdeknikker04@gmail.com>
2024-09-13 17:07:05 +0200

    Changes for hero in dark mode

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,7 +1,7 @@
 <div class="container hero-container col-12">
     <div class="row align-items-center py-5 hero-row">
         <div class="col-lg-6">
-            <h1 class="fw-bold text-body-emphasis lh-1">{{ $title }}</h1>
+            <h1 class="fw-bold lh-1">{{ $title }}</h1>
             <p class="description-paragraph">{{ $description }}</p>
             <div class="d-grid gap-2 d-md-flex justify-content-md-start button-hero-row">
                 <a href="{{ $first_button_url }}" class="btn btn-outline-primary btn-lg px-4 me-md-2 contact-hero-button">{{ $first_button }}</a>
