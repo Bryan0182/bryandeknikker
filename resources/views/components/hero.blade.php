a71577fafc9e4e954f8dabc5ea0970877dda45b6
Bryan0182 <bdeknikker04@gmail.com>
2024-09-13 22:28:01 +0200

    Changes for hero for mobile and desktop

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,5 +1,5 @@
 <div class="container hero-container col-12">
-    <div class="row align-items-center py-5 hero-row">
+    <div class="row align-items-center hero-row">
         <div class="col-lg-6">
             <h1 class="fw-bold lh-1">{{ $title }}</h1>
             <p class="description-paragraph">{{ $description }}</p>
@@ -8,7 +8,7 @@
                 <a href="{{ $second_button_url }}"  class="btn btn-primary btn-lg px-4 align-items-center d-flex read-more-hero-button">{{ $second_button }}</a>
             </div>
         </div>
-        <div class="col-12 col-lg-6 mt-tablet">
+        <div class="col-12 col-lg-6 mt-tablet image-column">
             <img src="{{ $imageSrc }}" class="d-block mx-auto ms-md-auto me-lg-0 img-fluid" alt="{{ $altText }}" width="{{ $width }}" height="{{ $height }}" loading="eager">
         </div>
     </div>
