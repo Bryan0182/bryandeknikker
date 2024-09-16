425c92e97df86f4c0d01ac649b37dae53b1e0ff2
Bryan0182 <bdeknikker04@gmail.com>
2024-09-16 10:40:23 +0200

    Changes for hero block

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -4,8 +4,13 @@
             <h1 class="fw-bold lh-1">{{ $title }}</h1>
             <p class="description-paragraph">{{ $description }}</p>
             <div class="d-grid gap-2 d-md-flex justify-content-md-start button-hero-row">
-                <a href="{{ $first_button_url }}" class="btn btn-outline-primary btn-lg px-4 me-md-2 contact-hero-button">{{ $first_button }}</a>
-                <a href="{{ $second_button_url }}"  class="btn btn-primary btn-lg px-4 align-items-center d-flex read-more-hero-button">{{ $second_button }}</a>
+                @if (!empty($first_button) && !empty($first_button_url))
+                    <a href="{{ $first_button_url }}" class="btn btn-outline-primary btn-lg px-4 me-md-2 contact-hero-button">{{ $first_button }}</a>
+                @endif
+
+                @if (!empty($second_button) && !empty($second_button_url))
+                    <a href="{{ $second_button_url }}" class="btn btn-primary btn-lg px-4 align-items-center d-flex read-more-hero-button">{{ $second_button }}</a>
+                @endif
             </div>
         </div>
         <div class="col-12 col-lg-6 mt-tablet image-column">
