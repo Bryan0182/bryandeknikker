9bb70f62654e2dac6aaa5e00c8e9291ac7662609
Bryan0182 <bdeknikker04@gmail.com>
2024-09-16 08:00:49 +0200

    Changes for base files

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- Begin pagina titel -->
-    <title>@yield('pageTitle', 'Bryan de Knikker') - Bryan de Knikker</title>
+    <title>@yield('title', 'Bryan de Knikker') - Bryan de Knikker</title>
     <!-- Einde pagina titel -->
 
     <!-- Begin favicon -->
