a147604db355159ba3a69d9655d0b81f19fb217d
Bryan0182 <bdeknikker04@gmail.com>
2024-09-16 10:40:32 +0200

    Changes for homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        // Verkrijg de 3 meest recente blogs
         $recentBlogs = Blog::orderBy('created_at', 'desc')->limit(3)->get();
 
         return view('pages.home', ['recentBlogs' => $recentBlogs]);
