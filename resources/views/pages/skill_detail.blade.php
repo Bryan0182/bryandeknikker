f28d4873bc32bdea0de34e6c8e5c4c06ebb74d60
Bryan0182 <bdeknikker04@gmail.com>
2024-09-30 07:57:52 +0200

    Changes for skills detail page

--- resources/views/pages/skill_detail.blade.php
+++ resources/views/pages/skill_detail.blade.php
@@ -11,15 +11,34 @@
 @endpush
 
 @section('content')
-    @component('components.text', [
+    @component('components.hero', [
         'title' => $skill->name,
-        'description' => $skill->description
+        'description' => $skill->description,
+        'first_button' => 'Contact',
+        'first_button_url' => '',
+        'second_button' => 'Lees meer',
+        'second_button_url' => '',
+        'imageSrc' => 'storage/' . $skill->icon,
+        'altText' => 'Foto van Bryan de Knikker',
+        'width' => 150,
+        'height' => 150,
+        'imageClass' => ''
     ])
     @endcomponent
 
     @component('components.text', [
         'title' => 'Skill Details',
         'description' => "{$skill->usage_count}<br><br>{$skill->proficiency}<br><br>{$skill->eager_to_learn}"
+    ])
+    @endcomponent
+
+    @component('components.cta', [
+        'title' => 'Benieuwd wat ik voor jou kan betekenen?',
+        'description' => 'Neem nu contact op',
+        'first_button' => 'Contact',
+        'first_button_url' => '/contact',
+        'second_button' => '',
+        'second_button_url' => ''
     ])
     @endcomponent
 @endsection
